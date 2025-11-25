e1ffbab963a64053feb29dcb6c0ecbb40cce7bae
Daniel Polonia <dpolonia@gmail.com>
2025-11-25 06:10:27 +0000

    Subir QR code para 24mm

--- deploy_publico/gerar_certificados_anonimos.py
+++ deploy_publico/gerar_certificados_anonimos.py
@@ -255,7 +255,7 @@ template_html = '''<!DOCTYPE html>
         
         .qr-code-container {
             position: absolute;
-            bottom: 21mm;
+            bottom: 24mm;
             right: 25mm;
             text-align: center;
             z-index: 20;
